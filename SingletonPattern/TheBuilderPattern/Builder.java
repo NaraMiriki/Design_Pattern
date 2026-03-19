a5a6b2d21cfba60c3245936af2645ffcf066e377
NaraMiriki <hominhchiqn0123@gmail.com>
2026-03-19 10:51:10 +0700

    Add files via upload

---
+++ SingletonPattern/TheBuilderPattern/Builder.java
@@ -0,0 +1,20 @@
+package ex4;
+
+public interface Builder {
+	void setBedType(String bedType);
+
+    void setWallColor(String wallColor);
+
+    void setFloorType(String floorType);
+
+    void setHasTV(boolean hasTV);
+
+    void setHasDesk(boolean hasDesk);
+
+    void setHasBalcony(boolean hasBalcony);
+
+    void setNumberOfWindows(int numberOfWindows);
+
+    void setLightingType(String lightingType);
+   
+}
